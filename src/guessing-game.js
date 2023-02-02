9dc4d8d785aa278b5b81b5a649abb8e8dbb5db1a
sergey-mak1 <sega013021990@gmail.com>
2023-02-03 01:48:35 +0300

    feat: add function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,41 @@
+
+let result
 class GuessingGame {
-    constructor() {}
+    constructor() { 
+        
+    }
 
     setRange(min, max) {
+        this.min = min
+        this.max = max
 
     }
 
     guess() {
-
+        this.result = Math.ceil((this.max + this.min) / 2)
+        result = this.result
+        console.log(result)
+        return result
+        
     }
-
+    
     lower() {
 
+        this.max = this.result;
     }
 
     greater() {
 
+       this.min = this.result ;
+       
     }
+
 }
 
+
+
+
+
+
 module.exports = GuessingGame;
+
